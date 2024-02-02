c492d3e7496cd8b9701758bbeb426b09825de1ba
Jeon SunYong <sunyong0725@naver.com>
2024-02-02 10:46:18 +0900

    모자른 부분

---
+++ CondingTest/part.js
@@ -0,0 +1,63 @@
+// for (let i = 0; i < 100; i++) {
+//   console.log(i);
+// }
+
+// let i = 0;
+
+// while (i < 100) {
+//   console.log(i);
+//   i++;
+// }
+
+// for (let i = 1; i < a; i++) {
+//   b.push(i);
+// }
+
+// console.log(b);
+
+// for (let i = 1; i <= a; i++) {
+//   if (i % 2 !== 0) {
+//     b.push(i);
+//   }
+// }
+
+// console.log(b);
+
+// for (let i = 0; i <= a; i++) {
+//   if (i % 5 === 0) {
+//     b.push(i);
+//   }
+// }
+
+// let c = b.slice(0, 5);
+
+// let a = 50;
+// let b = [];
+
+// for (let i = 0; i < a; i++) {
+//   console.log(`It is loop = ${b.push(i)}`);
+//   console.log(`It is loop = ${b.push(i)}`);
+//   console.log(`It is loop = ${b.push(i)}`);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j < 10; j++) {
+//     console.log(i, j);
+//   }
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   console.log(`number ${i} !`);
+//   i++;
+// }
+
+let user = ["sy", "tom", "dj"];
+
+user.forEach((item, index, arr) => {});
