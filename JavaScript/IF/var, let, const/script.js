6dae1d2ae096745773814d7ad7c17bd89efd2892
sy0725 <sunyong0725@naver.com>
2025-02-24 10:20:27 +0900

    var, let, const

---
+++ JavaScript/IF/var, let, const/script.js
@@ -0,0 +1,18 @@
+// 사용하지 않는다.
+var A = "Hello World";
+console.log(A);
+
+A = 123;
+
+console.log(A);
+
+// let은 중복 선언 불가, 재할당은 가능
+let B = "I'm SY";
+
+console.log(B);
+
+B = 725;
+console.log(B);
+
+// const는 중복 선언 불가, 재할당도 불가
+const C = "DANJI";
