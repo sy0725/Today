df3eae03a74bae58c8b5f7355e0bb1d7d6d87d01
sy0725 <sunyong0725@naver.com>
2025-02-24 11:43:26 +0900

    hoisting

---
+++ JavaScript/IF/hoisting/index.js
@@ -0,0 +1,9 @@
+// console.log(a);
+// console.log(b);
+// var a = 123; // Error가 나지 않고 undefined , 위로 호이스팅되기에 undefined 값을 할당
+// let b = 123; // Error
+
+// func();
+// function func() {
+//   console.log("hoisting test");   // hoisting test가 출력
+// }
