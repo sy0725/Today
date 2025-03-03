bb0139cadb6179b39c6035200f9f05a48ab50f8b
sy0725 <sunyong0725@naver.com>
2025-03-03 15:52:18 +0900

    이벤트 종류

--- JavaScript/IF/DOM/Dom Event/script.js
+++ JavaScript/IF/DOM/Dom Event/script.js
@@ -12,18 +12,62 @@
 //   alert("a element click");
 // });
 
-const buttonElement = document.querySelector(".btn2");
+// const buttonElement = document.querySelector(".btn2");
 
-buttonElement.addEventListener("click", (event) => {
-  let val;
-  val = event.target;
-  val = event.target.id;
-  val = event.target.className;
-  val = event.target.classList;
+// buttonElement.addEventListener("click", (event) => {
+//   let val;
+//   val = event.target;
+//   val = event.target.id;
+//   val = event.target.className;
+//   val = event.target.classList;
 
-  val = event.type;
-  val = event.clientY; // 윈도우로부터의 거리 좌표
-  val = event.offsetY; // 요소로부터의 거리 좌표
+//   val = event.type;
+//   val = event.clientY; // 윈도우로부터의 거리 좌표
+//   val = event.offsetY; // 요소로부터의 거리 좌표
 
-  console.log(val);
-});
+//   console.log(val);
+// });
+
+// 이벤트 종류
+
+const submitBtn = document.querySelector(".submitBtn");
+const form = document.querySelector("form");
+const title = document.querySelector("h2");
+
+// click event
+// submitBtn.addEventListener("click", handleEvent);
+// submitBtn.addEventListener("dblClick", handleEvent);
+// submitBtn.addEventListener("mousedown", handleEvent);
+// submitBtn.addEventListener("mouseup", handleEvent);
+// submitBtn.addEventListener("mouseenter", handleEvent);
+// submitBtn.addEventListener("mouseleave", handleEvent);
+// submitBtn.addEventListener("mousemove", handleEvent);
+
+// function handleEvent(e) {
+//   e.preventDefault(); // 기본 동작을 방지
+//   console.log(`Event Type : ${e.type}`);
+//   title.textContent = `MouseX : ${e.offsetX} , MouseY : ${e.offsetY}`;
+// }
+
+// form event
+
+const emailInput = document.getElementById("email");
+
+form.addEventListener("submit", handleEvent);
+emailInput.addEventListener("keydown", handleEvent);
+emailInput.addEventListener("keyup", handleEvent);
+emailInput.addEventListener("keypress", handleEvent);
+emailInput.addEventListener("focus", handleEvent);
+emailInput.addEventListener("blur", handleEvent);
+emailInput.addEventListener("cut", handleEvent);
+emailInput.addEventListener("paste", handleEvent);
+emailInput.addEventListener("input", handleEvent);
+
+function handleEvent(e) {
+  if (e.type === "submit") {
+    e.preventDefault();
+  }
+
+  console.log(`Event Type : ${e.type}`);
+  title.textContent = e.target.value;
+}
